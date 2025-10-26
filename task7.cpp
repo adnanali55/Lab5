6a6202fc62f1ba6a160b0c7510c863820f98222f
adnanali55 <f2025-0968@bnu.edu.pk>
2025-10-26 08:46:47 -0700

    Implement book purchase points calculation

---
+++ task7.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int books,points;
+    cout << "Enter the number of books purchased this month: ";
+    cin >> books;
+    if (books == 0)
+        points = 0;
+    else if (books == 1)
+        points = 5;
+    else if (books == 2)
+        points = 15;
+    else if (books == 3)
+        points = 30;
+    else if (books >= 4)
+        points = 60;
+    else {
+        cout << "Invalid input!" << endl;
+        return 1;
+    }
+    cout << "You have earned " << points << " points this month." << endl;
+
+    return 0;
+}
